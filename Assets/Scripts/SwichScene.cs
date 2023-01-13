74534901f98575f5a60328ee23de0c76df4b462e
Emblen <89460730+Kemblen@users.noreply.github.com>
2023-01-13 21:10:24 +0900

    add Scene change function

---
+++ Assets/Scripts/SwichScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwichScene : MonoBehaviour
+{
+
+    public string NextScene;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(NextScene, LoadSceneMode.Single);
+    }
+}
